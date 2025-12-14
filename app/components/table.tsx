adbe2174c42905e680a62c96f4c5d56230496c13
vikasgodara290 <vikasgodara290@gmail.com>
2025-12-14 22:51:32 +0530

    start and stop session button

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -15,29 +15,32 @@ export default function Table<T extends object>({columns, rows}: TableProps<T>){
 
             <table className="border border-black w-11/12 mx-auto">
             {
-                <tr>
-                    {
-                        columns.map(c => (
-                            <th key={c.key} className="border border-black">{c.label}</th>
-                        ))
-                    }
-                </tr>
-            }
-            {
-
-                rows.map(r => (
+                <thead>
                     <tr>
                         {
-                            Object.values(r).map((v, index) => {
-                                if(index == 0){ 
-                                    return <td className="hidden border border-black pl-1">{v}</td>
-                                }
-                                return <td className="border border-black pl-1">{v}</td>
-                            })
+                            columns.map(c => (
+                                <th key={c.key} className="border border-black">{c.label}</th>
+                            ))
                         }
                     </tr>
-                ))
+                </thead>
             }
+            <tbody>
+                {
+                    rows.map((r, index) => (
+                        <tr key={index}>
+                            {
+                                Object.values(r).map((v, index) => {
+                                    if(index == 0){ 
+                                        return <td key={index} className="hidden border border-black pl-1">{v}</td>
+                                    }
+                                    return <td key={index} className="border border-black pl-1">{v}</td>
+                                })
+                            }
+                        </tr>
+                    ))
+                }
+            </tbody>
             </table>
         </div>
     )
